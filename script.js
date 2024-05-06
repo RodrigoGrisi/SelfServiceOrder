daa5daa9bd75f2025043af83145ca232e3dbe17f
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-05-06 18:40:02 -0300

    Finish pick your items using Vue 3

--- script.js
+++ script.js
@@ -71,7 +71,22 @@ const selfServieMachine = {
       message: "Pick your items",
       products: window.products
     }
-  }
+  },
+  methods: {
+    total: function () {
+      let total = 0;
+
+      this.products.forEach(item => {
+        if (item.active) {
+          total += item.price * item.quantity
+        }
+
+      });
+      
+      return (total).toFixed(2);
+
+    }
+  },
 }
 
 Vue.createApp(selfServieMachine).mount('#app');
